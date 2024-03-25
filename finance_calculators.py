0d08c8cc36228589c9f39a4afd3b68c7da68012b
Max Godec <godecmax@gmail.com>
2024-03-25 17:19:12 +0000

    refactored code to include functions

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,15 +1,10 @@
 import math
 # project to help people calculate amounts of interest earned or bond to pay
-# user inputs whether they have an investment or a bond
-print("investment - to calculate the amount of interest you'll earn on your investment")
-print("bond       - to calculate the amount you'll have to pay on a home loan")
-print("")
 
-# user decides on investment or bond
-user_choice = input("Enter either 'investment' or 'bond' from the menu above to proceed: ")
+#--Functions--
 
-# user chooses investment - they are asked for amount deposited, interet rate, and years they will invest - they are then returned the profit
-if user_choice.lower() == "investment":
+#function to work out return on investment
+def investment():
     investment_return = 0
     investment_deposit = int(input("Enter amount being deposited: "))
     investment_interest = int(input("Enter interest rate: "))
@@ -21,15 +16,15 @@ if user_choice.lower() == "investment":
         investment_return += investment_deposit * (1 + (investment_interest/100 * investment_years))
         print(f"Your investment return will be: {investment_return - investment_deposit}")
 
-    elif interest_type == "compound":
+    if interest_type == "compound":
         investment_return += investment_deposit * math.pow((1 + investment_interest/100), investment_years)
         print(f"Your investment return will be: {investment_return - investment_deposit}")
 
     else:
         print("Error")
 
-# user chooses bond - they are asked for house value, interest and number of years to pay - monthly payments are returned        
-elif user_choice.lower() == "bond":
+#function to work out users bond payments
+def bond():
     monthly_repayments = 0
     house_value = int(input("Please enter the current value of the house: "))
     bond_interest = int(input("Please enter the interest rate: "))
@@ -38,6 +33,22 @@ elif user_choice.lower() == "bond":
     monthly_repayments += ((monthly_interest * house_value)) / (1 - (1 + monthly_interest) ** (-months_to_repay))
     print(f"Your monthly repayments are: {math.ceil(monthly_repayments)}.")
 
+#--Main program--
+
+# user inputs whether they have an investment or a bond
+print("Investment\tTo calculate the amount of interest you'll earn on your investment")
+print("Bond\t\tTo calculate the amount you'll have to pay on a home loan")
+print("")
+
+# user decides on investment or bond
+user_choice = input("Enter either 'Investment' or 'Bond' from the menu above to proceed: ")
+
+# user chooses investment 
+if user_choice.lower() == "investment":
+    investment()
+# user chooses bond       
+if user_choice.lower() == "bond":
+   bond()
 else:
     print("You have not input either 'investment' or 'bond', please try again.")
 
